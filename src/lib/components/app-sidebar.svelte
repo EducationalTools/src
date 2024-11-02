2bbb2cbda46d48b0a6a56d58eb2f17c69296ca26
Ingo <me@ingowolf.au>
2024-11-02 14:36:21 +1100

    Add user

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -1,5 +1,16 @@
 <script lang="ts">
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import * as Avatar from '$lib/components/ui/avatar/index.js';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+	import { useSidebar } from '$lib/components/ui/sidebar/index.js';
+
+	import School from 'svelte-material-icons/School.svelte';
+	import Account from 'svelte-material-icons/Account.svelte';
+	import Logout from 'svelte-material-icons/Logout.svelte';
+	import Delete from 'svelte-material-icons/Delete.svelte';
+	import Cog from 'svelte-material-icons/Cog.svelte';
+
+	const sidebar = useSidebar();
 </script>
 
 <Sidebar.Root variant="floating" collapsible="icon">
@@ -25,5 +36,48 @@
 		<Sidebar.Group />
 		<Sidebar.Group />
 	</Sidebar.Content>
-	<Sidebar.Footer />
+	<Sidebar.Footer>
+		<Sidebar.Menu>
+			<Sidebar.MenuItem>
+				<DropdownMenu.Root>
+					<DropdownMenu.Trigger>
+						{#snippet child({ props })}
+							<Sidebar.MenuButton
+								size="lg"
+								class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+								{...props}
+							>
+								<Avatar.Root class="h-8 w-8 rounded-lg">
+									<Avatar.Fallback class="rounded-lg"><Account /></Avatar.Fallback>
+								</Avatar.Root>
+								<div class="grid flex-1 text-left text-sm leading-tight">
+									<span class="truncate font-semibold">Name</span>
+								</div>
+								<!-- <ChevronsUpDown class="ml-auto size-4" /> -->
+							</Sidebar.MenuButton>
+						{/snippet}
+					</DropdownMenu.Trigger>
+					<DropdownMenu.Content
+						class="w-[--bits-dropdown-menu-anchor-width] min-w-56 rounded-lg"
+						side={sidebar.isMobile ? 'bottom' : 'right'}
+						align="end"
+						sideOffset={4}
+					>
+						<DropdownMenu.Item>
+							<Cog />
+							Settings
+						</DropdownMenu.Item>
+						<DropdownMenu.Item>
+							<Logout />
+							Log out
+						</DropdownMenu.Item>
+						<DropdownMenu.Item>
+							<Delete />
+							Delete account
+						</DropdownMenu.Item>
+					</DropdownMenu.Content>
+				</DropdownMenu.Root>
+			</Sidebar.MenuItem>
+		</Sidebar.Menu>
+	</Sidebar.Footer>
 </Sidebar.Root>
