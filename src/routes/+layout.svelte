4187699d44b241e4801c7dbb24bfd6a65d4a0fad
Inglan <Inglan@users.noreply.github.com>
2024-12-17 02:12:28 +0000

    Prettified Code!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,9 +4,7 @@
 
 	import AppSidebar from '$lib/components/app-sidebar.svelte';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
-
 </script>
-
 
 <Sidebar.Provider>
 	<AppSidebar />
