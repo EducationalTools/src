16755d05a0380a367afebdb5c651f15b2e0a9051
Ingo <me@ingowolf.au>
2024-12-17 12:31:36 +1100

    add command to layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,16 @@
 
 	import AppSidebar from '$lib/components/app-sidebar.svelte';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import * as Command from '$lib/components/ui/command/index.js';
+
+	let open = $state(false);
+
+	function handleKeydown(e: KeyboardEvent) {
+		if (e.key === 'j' && (e.metaKey || e.ctrlKey)) {
+			e.preventDefault();
+			open = !open;
+		}
+	}
 </script>
 
 <Sidebar.Provider>
