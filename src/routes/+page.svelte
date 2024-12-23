0298088c6597b616e02d2d51d1b2a420f5123dab
Ingo <me@ingowolf.au>
2024-12-23 17:44:02 +1100

    add test code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,23 @@
 <script lang="ts">
 	import { testStore } from '$lib/state';
+	import { Switch } from '$lib/components/ui/switch/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
+
+	let test;
+
+	testStore.subscribe(($store) => {
+		test = $store;
+	});
 </script>
+
+<Button
+	onclick={function () {
+		testStore.set(true);
+	}}>true</Button
+>
+<Button
+	onclick={function () {
+		testStore.set(false);
+	}}>false</Button
+>
+<span>{test}</span>
