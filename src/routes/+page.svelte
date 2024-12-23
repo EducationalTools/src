825257ee81cf7c50fcc040e0dbcaf320d3922ea4
Ingo <me@ingowolf.au>
2024-12-23 18:12:31 +1100

    finally find how to make this work easier svelte is awesome thanks mozilla mdn docs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,24 +2,10 @@
 	import { testStore } from '$lib/state';
 	import { Switch } from '$lib/components/ui/switch/index.js';
 	import { Button } from '$lib/components/ui/button/index.js';
-
-	let test: boolean;
-
-	testStore.subscribe(($store) => {
-		test = $store;
-	});
 </script>
 
-<Button
-	onclick={function () {
-		testStore.set(true);
-	}}>true</Button
->
-<Button
-	onclick={function () {
-		testStore.set(false);
-	}}>false</Button
->
-<span>{test}</span>
+{$testStore}
 
-<Switch />
+<Switch bind:checked={$testStore} />
+
+<Button onclick={() => testStore.set(!$testStore)}>Toggle</Button>
