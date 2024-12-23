a8539bd2c4bf1689649de326f5797f0ce10db7e0
Ingo <me@ingowolf.au>
2024-12-23 18:21:19 +1100

    remove everything from homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,2 @@
 <script lang="ts">
-	import { testStore } from '$lib/state';
-	import { Switch } from '$lib/components/ui/switch/index.js';
-	import { Button } from '$lib/components/ui/button/index.js';
 </script>
-
-{$testStore}
-
-<Switch bind:checked={$testStore} />
-
-<Button onclick={() => testStore.set(!$testStore)}>Toggle</Button>
