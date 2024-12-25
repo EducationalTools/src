cb3cbcfc4c4b5d0d2959eaa569e9efed2c8cabb0
Ingo <me@ingowolf.au>
2024-12-25 17:12:21 +1100

    add UI for password generator

---
+++ src/routes/tools/password-generator/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+</script>
+
+<div class="flex h-full flex-row items-center justify-center gap-3 p-3">
+	<Input />
+	<Button>Generate</Button>
+	<Button>Copy</Button>
+</div>
