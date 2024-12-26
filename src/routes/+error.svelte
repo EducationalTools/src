54af4039f2bedfb5a1eaac16cd059061b7e55c43
Ingo <me@ingowolf.au>
2024-12-26 12:33:12 +1100

    add 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import { Button } from '$lib/components/ui/button/index.js';
+</script>
+
+<div class="flex h-full flex-col items-center justify-center gap-3">
+	<span class="text-6xl">{page.status}</span>
+	<span class="text-3xl">{page.error?.message}</span>
+	<Button variant="outline" href="/">Go home</Button>
+</div>
