8c8658c321e8e423172feb0831a59293f173cf3b
Ingo <me@ingowolf.au>
2024-12-30 12:51:26 +1100

    add sample page

--- src/routes/g/[id]/+page.svelte
+++ src/routes/g/[id]/+page.svelte
@@ -2,4 +2,6 @@
 	import { page } from '$app/state';
 </script>
 
-{page.params.id}
+<div class="flex h-full w-full flex-row gap-3 p-3">
+	<iframe src="https://example.com" frameborder="0" class="flex-grow rounded"></iframe>
+</div>
