49a992a2ecb1d05ae617221935d9fbf7ec3da2c7
Ingo <me@ingowolf.au>
2024-12-30 12:54:02 +1100

    add some more testing code

--- src/routes/g/[id]/+page.svelte
+++ src/routes/g/[id]/+page.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
 	import { page } from '$app/state';
+	import { getGameById } from '$lib/gmaes';
+
+	const gmaedata = $derived(getGameById(page.params.id));
 </script>
 
 <div class="flex h-full w-full flex-row gap-3 p-3">
 	<iframe src="https://example.com" frameborder="0" class="flex-grow rounded"></iframe>
+
+	{JSON.stringify(gmaedata)}
 </div>
