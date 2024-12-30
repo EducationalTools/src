c14bec3fa4e7f18069f62eb746978330fb230395
Ingo <me@ingowolf.au>
2024-12-30 13:00:13 +1100

    update iframe source and title to use dynamic data

--- src/routes/g/[id]/+page.svelte
+++ src/routes/g/[id]/+page.svelte
@@ -6,7 +6,8 @@
 </script>
 
 <div class="flex h-full w-full flex-row gap-3 p-3">
-	<iframe src="https://example.com" frameborder="0" class="flex-grow rounded"></iframe>
+	<iframe src={gmaedata?.url} frameborder="0" class="flex-grow rounded" title={gmaedata?.name}
+	></iframe>
 
 	{JSON.stringify(gmaedata)}
 </div>
