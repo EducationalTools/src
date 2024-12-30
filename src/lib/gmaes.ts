894e4f35710e972ee2171e49f4cee28b8e3c85de
Ingo <me@ingowolf.au>
2024-12-30 12:06:00 +1100

    add storage for stuff

---
+++ src/lib/gmaes.ts
@@ -0,0 +1,18 @@
+export const games = [
+	{
+		id: 'test',
+		name: 'Test Game',
+		description: 'This is a test game',
+		url: '/_app/tools/test/index.html'
+	},
+	{
+		id: 'test2',
+		name: 'Test Game 2',
+		description: 'This is a test game 2',
+		url: '/_app/tools/test2/index.html'
+	}
+];
+
+export function getGameById(id: string) {
+	return games.find((game) => game.id === id);
+}
