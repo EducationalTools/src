ca1614aa5b6580f0883e8b728204ff12db844b5f
Ingo <me@ingowolf.au>
2025-01-03 17:58:59 +1100

    add stores

--- src/lib/state.ts
+++ src/lib/state.ts
@@ -4,3 +4,6 @@ export const preferencesStore = persisted('preferences', {
 	experimentalFeatures: false,
 	open: 'tab'
 });
+
+export const favoritesStore = persisted('favorites', [] as string[]);
+export const historyStore = persisted('history', [] as string[]);
