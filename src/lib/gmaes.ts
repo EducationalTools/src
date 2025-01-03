87d64d2cec2d36479e0f4ebb5d486fad930fa4a1
Ingo <me@ingowolf.au>
2025-01-03 17:07:31 +1100

    refactor gmaes to use generateId for unique identifiers

--- src/lib/gmaes.ts
+++ src/lib/gmaes.ts
@@ -1,13 +1,13 @@
-export const gmaes = [
+import { generate, generate as generateId } from './idgen';
+
+const gmaes = [
 	{
-		id: '2048',
 		name: '2048',
 		description:
 			'Press arrow keys randomly until you get the 2048 tile. Then continue pressing arrow keys randomly.',
 		url: '/_app/tools/2048/index.html'
 	},
 	{
-		id: 'bitlife',
 		name: 'Bitlife',
 		description: 'Do stupid stuff with a virtual character or something.',
 		url: '/_app/tools/bitlife/index.html'
@@ -17,3 +17,16 @@ export const gmaes = [
 export function getGameById(id: string) {
 	return gmaes.find((gmae) => gmae.id === id);
 }
+
+let parsedGmaes = [];
+
+gmaes.forEach((gmae) => {
+	parsedGmaes.push({
+		id: generateId(gmae.name),
+		name: gmae.name,
+		description: gmae.description,
+		url: gmae.url
+	});
+});
+
+export { parsedGmaes as games };
