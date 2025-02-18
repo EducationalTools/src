372ad20b3d29afe5b2c3714e90d2f3f219d45dd5
Ingo <me@ingowolf.au>
2025-02-18 16:25:57 +1100

    add tipex

--- src/routes/tools/simple-text-editor/+page.svelte
+++ src/routes/tools/simple-text-editor/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import { Textarea } from '$lib/components/ui/textarea/index.js';
 	import * as Alert from '$lib/components/ui/alert/index.js';
+
+	import { Tipex } from '@friendofsvelte/tipex';
+	let body = `<p>The initial html content.</p>`;
 </script>
 
 <div class="flex h-full flex-col gap-3 p-3">
@@ -10,5 +13,13 @@
 		>
 		<Alert.Description>If you know what you are doing open a pr :)</Alert.Description>
 	</Alert.Root>
-	<Textarea class="flex-grow resize-none"></Textarea>
+	<!-- <Textarea class="flex-grow resize-none"></Textarea> -->
+	<Tipex
+		{body}
+		controls
+		floating
+		focal
+		style="margin-top: 1rem; margin-bottom: 0;"
+		class="h-[70vh] border border-neutral-200"
+	/>
 </div>
