1227952b908e087a212eef4a67b7aa36b80eccef
Ingo Wolf <me@ingowolf.au>
2025-03-25 16:21:17 +1100

    add settings dialog component

---
+++ src/lib/components/settings.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import * as Dialog from '$lib/components/ui/dialog/index.js';
+
+	let settingsOpen = $state(false);
+</script>
+
+<Dialog.Root bind:open={settingsOpen}>
+	<Dialog.Content>
+		<Dialog.Header>
+			<Dialog.Title>Settings</Dialog.Title>
+		</Dialog.Header>
+	</Dialog.Content>
+</Dialog.Root>
