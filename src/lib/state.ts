88521fce067c98f17aa3ae521f1cd0be99c90d99
Ingo Wolf <me@ingowolf.au>
2025-03-30 17:55:52 +1100

    add some settings

--- src/lib/state.ts
+++ src/lib/state.ts
@@ -2,7 +2,21 @@ import { persisted } from 'svelte-persisted-store';
 
 export const preferencesStore = persisted('preferences', {
 	experimentalFeatures: false,
-	open: 'tab'
+	open: 'tab',
+	theme: 'dark',
+	panic: {
+		enabled: false,
+		key: '`',
+		url: 'https://classroom.google.com',
+		disableExperimentalMode: true
+	},
+	cloak: {
+		mode: 'off',
+		name: 'Home',
+		icon: 'https://ssl.gstatic.com/classroom/favicon.png'
+	},
+	analytics: true,
+	history: true
 });
 
 export const favoritesStore = persisted('favorites', [] as string[]);
