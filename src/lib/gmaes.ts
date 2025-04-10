b854281329c914753b12874765cc3cfd8ab47d1f
Ingo Wolf <me@ingowolf.au>
2025-04-10 19:17:41 +1000

    AI adds gmaes...

--- src/lib/gmaes.ts
+++ src/lib/gmaes.ts
@@ -23,15 +23,140 @@ const gmaes = [
 		url: '/_app/tools/crappybird/index.html'
 	},
 	{
+		name: 'Fake Paper.io 2',
+		description: 'Capture territory in this multiplayer IO game.',
+		url: '/_app/tools/fakepaperio2/index.html'
+	},
+	{
+		name: 'Floppy Bird',
+		description: 'Guide a bird through pipes by tapping to flap its wings.',
+		url: '/_app/tools/floppybird/index.html'
+	},
+	{
+		name: 'Fruit Ninja',
+		description: 'Slice fruit with your finger while avoiding bombs.',
+		url: '/_app/tools/fruitninja/index.html'
+	},
+	{
+		name: 'Grindcraft Remastered',
+		description: 'Mine resources and craft items in this idle game.',
+		url: '/_app/tools/grindcraftremastered/index.html'
+	},
+	{
+		name: 'Hole.io',
+		description: 'Consume objects with your black hole to grow bigger.',
+		url: '/_app/tools/holeio/index.html'
+	},
+	{
+		name: 'House of Hazards',
+		description: 'Navigate through hazardous house environments.',
+		url: '/_app/tools/houseofhazards/index.html'
+	},
+	{
+		name: 'Idle Breakout',
+		description: 'Break bricks automatically in this idle game version of Breakout.',
+		url: '/_app/tools/idlebreakout/index.html'
+	},
+	{
+		name: 'Mario Kart',
+		description: 'Race as Mario characters on various tracks.',
+		url: '/_app/tools/mariokart/index.html'
+	},
+	{
+		name: 'Minecraft',
+		description: 'Build and explore in this blocky world.',
+		url: '/_app/tools/minecraft/index.html'
+	},
+	{
+		name: 'Minecraft Trial',
+		description: 'Try out the Minecraft experience.',
+		url: '/_app/tools/minecrafttrial/index.html'
+	},
+	{
 		name: 'Monkey Mart',
 		description: 'Monkeys have feelings to you know',
 		url: '/_app/tools/monkeymart/index.html'
+	},
+	{
+		name: 'Monopoly',
+		description: 'Buy properties and bankrupt your opponents in this classic board game.',
+		url: '/_app/tools/monopoly/index.html'
+	},
+	{
+		name: 'Paper.io 2',
+		description: 'Capture territory in this IO game by drawing boundaries.',
+		url: '/_app/tools/paperio2/index.html'
+	},
+	{
+		name: 'Pokemon Red',
+		description: 'Play the classic Pokemon Red game on your browser.',
+		url: '/_app/tools/pokemonred/index.html'
+	},
+	{
+		name: 'PTTM',
+		description: 'Play this mysterious game (PTTM).',
+		url: '/_app/tools/pttm/index.html'
+	},
+	{
+		name: 'Rocket League',
+		description: 'Play soccer with rocket-powered cars.',
+		url: '/_app/tools/rocketleague/index.html'
+	},
+	{
+		name: 'Scrab',
+		description: 'Form words from letter tiles in this word game.',
+		url: '/_app/tools/scrab/index.html'
+	},
+	{
+		name: 'Slenderman',
+		description: 'Collect pages while avoiding the Slenderman in this horror game.',
+		url: '/_app/tools/slenderman/index.html'
 	},
 	{
 		name: 'Slope',
 		description:
 			'A game where you roll a ball down a slope until you either fall off or touch red (skill issue',
 		url: '/_app/tools/slope/index.html'
+	},
+	{
+		name: 'Snow Rider',
+		description: 'Ride down snowy slopes, performing tricks and avoiding obstacles.',
+		url: '/_app/tools/snowrider/index.html'
+	},
+	{
+		name: 'Solitaire',
+		description: 'Play the classic card game of patience.',
+		url: '/_app/tools/solit/index.html'
+	},
+	{
+		name: 'SPR',
+		description: 'Play this SPR game.',
+		url: '/_app/tools/spr/index.html'
+	},
+	{
+		name: 'SUPERHOT',
+		description: 'Time moves only when you move in this FPS game.',
+		url: '/_app/tools/superhot/index.html'
+	},
+	{
+		name: 'SW Surfer',
+		description: 'Surf through this Star Wars themed endless runner.',
+		url: '/_app/tools/swsurfer/index.html'
+	},
+	{
+		name: 'Tetris',
+		description: 'Arrange falling blocks to create complete lines.',
+		url: '/_app/tools/tetris/index.html'
+	},
+	{
+		name: 'Tic Tac Toe',
+		description: 'Play the classic game of X and O.',
+		url: '/_app/tools/tictactoe/index.html'
+	},
+	{
+		name: 'XT Pamplona',
+		description: 'Run with the bulls in this game based on the famous Spanish festival.',
+		url: '/_app/tools/xtpamplona/index.html'
 	}
 ];
 
