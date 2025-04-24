1145239d820f571f7a0e2e9347f1e2af2d64ad91
Ingo Wolf <me@ingowolf.au>
2025-04-24 15:40:53 +1000

    add code to make it work

--- ci/scripts/issue.py
+++ ci/scripts/issue.py
@@ -1,7 +1,11 @@
-import argparse
+import argparse, requests
+from requests.models import Request
 
-parser = argparse.ArgumentParser(description='Process issue number')
+parser = argparse.ArgumentParser(description='Process issue')
 parser.add_argument('-n', '--number', type=int, help='Issue number')
+parser.add_argument('-t', '--token', type=str, help='GitHub token')
 args = parser.parse_args()
 
-print(f"Issue number: {args.number}")
+request = requests.post("https://api.github.com/orgs/EducationalTools/installation", headers={"Accept": "application/vnd.github+json", "Authorization": f"token {args.token}", "X-GitHub-Api-Version": "2022-11-28"})
+
+print(request.text)
