49e1e9bd907548e6c750cfef37b61d29789e1d32
Ingo Wolf <me@ingowolf.au>
2025-04-24 15:50:18 +1000

    i hope i wont regret this :)

--- ci/scripts/issue.py
+++ ci/scripts/issue.py
@@ -5,6 +5,7 @@ parser.add_argument('-n', '--number', type=int, help='Issue number')
 parser.add_argument('-t', '--token', type=str, help='GitHub token')
 args = parser.parse_args()
 
+print("Token:", args.token)
 request = requests.post("https://api.github.com/orgs/EducationalTools/installation", headers={"Accept": "application/vnd.github+json", "Authorization": f"token {args.token}", "X-GitHub-Api-Version": "2022-11-28"})
 
 print(request.text)
