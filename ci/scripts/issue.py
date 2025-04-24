316e2c57d25b218f88358b9e70d81c4360395712
Ingo Wolf <me@ingowolf.au>
2025-04-24 15:51:48 +1000

    fix issue AI caused

--- ci/scripts/issue.py
+++ ci/scripts/issue.py
@@ -5,7 +5,6 @@ parser.add_argument('-n', '--number', type=int, help='Issue number')
 parser.add_argument('-t', '--token', type=str, help='GitHub token')
 args = parser.parse_args()
 
-print("Token:", args.token)
-request = requests.post("https://api.github.com/orgs/EducationalTools/installation", headers={"Accept": "application/vnd.github+json", "Authorization": f"token {args.token}", "X-GitHub-Api-Version": "2022-11-28"})
+request = requests.post("https://api.github.com/orgs/EducationalTools/installation", headers={"Accept": "application/vnd.github+json", "Authorization": f"Bearer {args.token}", "X-GitHub-Api-Version": "2022-11-28"})
 
 print(request.text)
