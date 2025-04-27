709ba91732b436a8b98a858f2937bb214e1d1f65
Ingo Wolf <me@ingowolf.au>
2025-04-27 10:11:54 +1000

    add code to make it do stuff

--- ci/scripts/issue.py
+++ ci/scripts/issue.py
@@ -5,32 +5,30 @@ parser.add_argument('-n', '--number', type=int, help='Issue number')
 parser.add_argument('-t', '--token', type=str, help='GitHub token')
 args = parser.parse_args()
 
-if args.number:
-    issue_request = requests.get(
-        f"https://api.github.com/repos/EducationalTools/src/issues/{args.number}",
+def issue_comment(body):
+    comment_request = requests.post(
+        f"https://api.github.com/repos/EducationalTools/src/issues/{args.number}/comments",
         headers={
             "Accept": "application/vnd.github+json",
             "Authorization": f"Bearer {args.token}",
             "X-GitHub-Api-Version": "2022-11-28"
-        }
+        },
+        json={"body": body}
     )
+    return comment_request
 
-    if issue_request.status_code == 200:
-        comment_request = requests.post(
-            f"https://api.github.com/repos/EducationalTools/src/issues/{args.number}/comments",
-            headers={
-                "Accept": "application/vnd.github+json",
-                "Authorization": f"Bearer {args.token}",
-                "X-GitHub-Api-Version": "2022-11-28"
-            },
-            json={"body": "body: " + issue_request.json()["body"]}
-        )
-        if comment_request.status_code == 201:
-            print("Comment added successfully")
-        else:
-            print(f"Failed to add comment: {comment_request.status_code}")
-    else:
-        print(f"Failed to get issue: {issue_request.status_code}")
+issue_request = requests.get(
+    f"https://api.github.com/repos/EducationalTools/src/issues/{args.number}",
+    headers={
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {args.token}",
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+)
 
-    print(f"Issue {args.number} details:")
-    print(issue_request.json())
+if not issue_request.status_code == 200:
+    print(f"Issue {args.number} not found")
+    exit(1)
+
+if issue_request.json()["title"].startswith("[Gmae Request] "):
+    issue_comment("this is a gmae request")
