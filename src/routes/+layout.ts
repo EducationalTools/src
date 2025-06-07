79af9e6e969e33f673817122f5e1b1425c21e6b4
Ingo Wolf <me@ingowolf.au>
2025-06-07 23:45:51 +1000

    move

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,13 +1,1 @@
 export const prerender = false;
-import posthog from 'posthog-js';
-import { browser } from '$app/environment';
-import { onMount } from 'svelte';
-
-onMount(() => {
-	if (browser) {
-		posthog.init('phc_jg4gOdigfHQD4MSgrSaO883dp2LjNJbJO7azv61UtI0', {
-			api_host: 'https://us.i.posthog.com',
-			person_profiles: 'always'
-		});
-	}
-});
