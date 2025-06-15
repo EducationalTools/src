5fd8afd5619be217cb430070c5e8833bcd366af6
Ingo Wolf <me@ingowolf.au>
2025-06-15 16:28:38 +1000

    change styling (mostly for mobile

--- src/lib/components/settings.svelte
+++ src/lib/components/settings.svelte
@@ -20,73 +20,75 @@
 </script>
 
 <Dialog.Root bind:open={settingsOpen.current}>
-	<Dialog.Content>
-		<Dialog.Header>
-			<Dialog.Title>Settings</Dialog.Title>
-		</Dialog.Header>
-		Open in
-		<RadioGroup.Root bind:value={$preferencesStore.open}>
-			<div class="flex items-center space-x-2">
-				<RadioGroup.Item value="tab" id="tab" />
-				<Label for="tab">Tab</Label>
-			</div>
-			<div class="flex items-center space-x-2">
-				<RadioGroup.Item value="window" id="window" />
-				<Label for="window">Window</Label>
-			</div>
-		</RadioGroup.Root>
-		Themes
-		<Dialog.Root>
-			<Dialog.Trigger class={clsx(buttonVariants({ variant: 'outline' }), 'w-fit justify-start')}>
-				{themeTriggerContent}
-				<ChevronDownIcon class="opacity-50" />
-			</Dialog.Trigger>
-			<Dialog.Content class="p-0">
-				<div class="max-h-[80vh] overflow-auto p-6">
-					<RadioGroup.Root bind:value={$preferencesStore.theme}>
-						{#each themes as theme}
-							<div class="flex items-center space-x-2">
-								<RadioGroup.Item value={theme.value} id={theme.value} />
-								<Label for={theme.value}>{theme.label}</Label>
-							</div>
-						{/each}
-					</RadioGroup.Root>
+	<Dialog.Content class="p-0">
+		<div class="flex max-h-[80vh] flex-col gap-4 overflow-auto p-6">
+			<Dialog.Header>
+				<Dialog.Title>Settings</Dialog.Title>
+			</Dialog.Header>
+			Open in
+			<RadioGroup.Root bind:value={$preferencesStore.open}>
+				<div class="flex items-center space-x-2">
+					<RadioGroup.Item value="tab" id="tab" />
+					<Label for="tab">Tab</Label>
 				</div>
-			</Dialog.Content>
-		</Dialog.Root>
-		Panic key
-		<div class="flex items-center gap-3">
-			<Checkbox id="panic" bind:checked={$preferencesStore.panic.enabled} />
-			<Label for="panic">Enable Panic Key</Label>
-		</div>
-		<div class="flex items-center gap-3">
-			<Checkbox id="panic" bind:checked={$preferencesStore.panic.enabled} />
-			<Label for="panic">Disable Experimental Mode when triggered</Label>
-		</div>
-		<div class="flex flex-row gap-3">
-			<Input bind:value={$preferencesStore.panic.key} placeholder="Key" maxlength={1} />
-			<Input bind:value={$preferencesStore.panic.url} placeholder="URL" type="url" />
-		</div>
-		Cloak
-		<Tabs.Root bind:value={$preferencesStore.cloak.mode}>
-			<Tabs.List class="w-full">
-				<Tabs.Trigger value="off">Off</Tabs.Trigger>
-				<Tabs.Trigger value="blur">When not focused</Tabs.Trigger>
-				<Tabs.Trigger value="on">On</Tabs.Trigger>
-			</Tabs.List>
-		</Tabs.Root>
-		<div class="flex flex-row gap-3">
-			<Input bind:value={$preferencesStore.cloak.name} placeholder="Page Name" />
-			<Input bind:value={$preferencesStore.cloak.icon} placeholder="Icon URL" />
-		</div>
-		Privacy
-		<div class="flex items-center gap-3">
-			<Checkbox id="analytics" bind:checked={$preferencesStore.analytics} />
-			<Label for="analytics">Enable Analytics</Label>
-		</div>
-		<div class="flex items-center gap-3">
-			<Checkbox id="history" bind:checked={$preferencesStore.history} />
-			<Label for="history">Enable History</Label>
+				<div class="flex items-center space-x-2">
+					<RadioGroup.Item value="window" id="window" />
+					<Label for="window">Window</Label>
+				</div>
+			</RadioGroup.Root>
+			Themes
+			<Dialog.Root>
+				<Dialog.Trigger class={clsx(buttonVariants({ variant: 'outline' }), 'w-fit justify-start')}>
+					{themeTriggerContent}
+					<ChevronDownIcon class="opacity-50" />
+				</Dialog.Trigger>
+				<Dialog.Content class="p-0">
+					<div class="max-h-[80vh] overflow-auto p-6">
+						<RadioGroup.Root bind:value={$preferencesStore.theme}>
+							{#each themes as theme}
+								<div class="flex items-center space-x-2">
+									<RadioGroup.Item value={theme.value} id={theme.value} />
+									<Label for={theme.value}>{theme.label}</Label>
+								</div>
+							{/each}
+						</RadioGroup.Root>
+					</div>
+				</Dialog.Content>
+			</Dialog.Root>
+			Panic key
+			<div class="flex items-center gap-3">
+				<Checkbox id="panic" bind:checked={$preferencesStore.panic.enabled} />
+				<Label for="panic">Enable Panic Key</Label>
+			</div>
+			<div class="flex items-center gap-3">
+				<Checkbox id="panic" bind:checked={$preferencesStore.panic.enabled} />
+				<Label for="panic">Disable Experimental Mode when triggered</Label>
+			</div>
+			<div class="flex flex-row gap-3">
+				<Input bind:value={$preferencesStore.panic.key} placeholder="Key" maxlength={1} />
+				<Input bind:value={$preferencesStore.panic.url} placeholder="URL" type="url" />
+			</div>
+			Cloak
+			<Tabs.Root bind:value={$preferencesStore.cloak.mode}>
+				<Tabs.List class="w-full">
+					<Tabs.Trigger value="off">Off</Tabs.Trigger>
+					<Tabs.Trigger value="blur">When not focused</Tabs.Trigger>
+					<Tabs.Trigger value="on">On</Tabs.Trigger>
+				</Tabs.List>
+			</Tabs.Root>
+			<div class="flex flex-row gap-3">
+				<Input bind:value={$preferencesStore.cloak.name} placeholder="Page Name" />
+				<Input bind:value={$preferencesStore.cloak.icon} placeholder="Icon URL" />
+			</div>
+			Privacy
+			<div class="flex items-center gap-3">
+				<Checkbox id="analytics" bind:checked={$preferencesStore.analytics} />
+				<Label for="analytics">Enable Analytics</Label>
+			</div>
+			<div class="flex items-center gap-3">
+				<Checkbox id="history" bind:checked={$preferencesStore.history} />
+				<Label for="history">Enable History</Label>
+			</div>
 		</div>
 	</Dialog.Content>
 </Dialog.Root>
