35fc86db49372165460e82c5c07f5d721aa8124d
Ingo Wolf <me@ingowolf.au>
2025-06-16 13:17:49 +1000

    add focused state and simplify

--- src/lib/components/cloak.svelte
+++ src/lib/components/cloak.svelte
@@ -1,12 +1,25 @@
 <script lang="ts">
 	import { preferencesStore } from '$lib/stores';
+	import { onMount } from 'svelte';
+
+	let focused = $state(false);
 
 	let title = $derived(
-		($preferencesStore.cloak.mode == 'on' && $preferencesStore.cloak.name) || 'EduTools'
+		$preferencesStore.cloak.mode == 'on' ? $preferencesStore.cloak.name : 'EduTools'
 	);
 	let icon = $derived(
 		$preferencesStore.cloak.mode == 'on' ? $preferencesStore.cloak.icon : '/favicon.ico'
 	);
+
+	onMount(() => {
+		document.addEventListener('blur', () => {
+			focused = false;
+		});
+
+		document.addEventListener('focus', () => {
+			focused = true;
+		});
+	});
 </script>
 
 <svelte:head>
