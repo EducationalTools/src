d2e984fc1cf0f714b86c68b66e05771d561937b1
Ingo Wolf <me@ingowolf.au>
2025-06-16 13:41:59 +1000

    add sidebar shortcut (whoops wrong branch lol)

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -296,6 +296,12 @@
 			>
 				<PanelLeft />
 				Sidebar
+				<div class="grow"></div>
+				<kbd
+					class="bg-muted text-muted-foreground pointer-events-none inline-flex h-5 items-center gap-1 rounded border px-1.5 font-mono text-[10px] font-medium opacity-100 select-none"
+				>
+					<span class="text-xs">⌘</span>B
+				</kbd>
 			</Sidebar.MenuButton>
 		</Sidebar.MenuItem>
 	</Sidebar.Footer>
