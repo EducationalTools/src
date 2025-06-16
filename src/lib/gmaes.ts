93dd5da657cdc519fbdfab9045efe954fecbaf4b
Ingo Wolf <me@ingowolf.au>
2025-06-16 14:21:06 +1000

    add the final earth

--- src/lib/gmaes.ts
+++ src/lib/gmaes.ts
@@ -345,6 +345,14 @@ const gmaes: Gmae[] = [
 		tags: [],
 		links: [],
 		url: '/_app/tools/achievementunlocked/index.html'
+	},
+	{
+		name: 'The Final Earth',
+		description: '',
+		category: 'Sandbox',
+		tags: [],
+		links: [],
+		url: '/_app/tools/the-final-earth/index.html'
 	}
 ];
 
