837a360d4e5253622b124269d943430737ef5ed7
Ingo Wolf <me@ingowolf.au>
2025-06-17 15:56:08 +1000

    add catppuccin darker

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -2,6 +2,7 @@ export const themes = [
 	{ value: 'shadcn-zinc', label: 'Shadcn Zinc' },
 	{ value: 'black-white', label: 'Black & White' },
 	{ value: 'catppuccin', label: 'Catppuccin' },
+	{ value: 'catppuccin-darker', label: 'Catppuccin Darker' },
 	{ value: 'gruvbox', label: 'Gruvbox' },
 	{ value: 'solarised', label: 'Solarised' },
 	{ value: 'github', label: 'GitHub' },
