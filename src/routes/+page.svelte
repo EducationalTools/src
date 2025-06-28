4f404df9145276f8c5ef09876204807d92d6c852
Ingo Wolf <me@ingowolf.au>
2025-06-28 16:53:53 +1000

    add start of home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
 <script lang="ts">
+	import { preferencesStore } from '$lib/stores';
 </script>
+
+<div
+	class="from-accent/50 flex w-full items-center justify-center bg-gradient-to-b to-transparent py-52"
+>
+	<h1 class="text-4xl">
+		EduTools {#if $preferencesStore.experimentalFeatures}Experimental{/if}
+	</h1>
+</div>
