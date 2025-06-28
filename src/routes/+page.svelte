c0640ec3c5ae41559c35ca532133ce9580f78d10
Ingo Wolf <me@ingowolf.au>
2025-06-28 17:00:15 +1000

    add tools

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
 	import { preferencesStore } from '$lib/stores';
+
+	const tools = [
+		{ name: 'Calculator', url: '/tools/calculator' },
+		{ name: 'Converter', url: '/tools/converter' },
+		{ name: 'Rich Text Editor', url: '/tools/rich-text-editor' },
+		{ name: 'Word Counter', url: '/tools/word-counter' },
+		{ name: 'Password Generator', url: '/tools/password-generator' },
+		{ name: 'Random Number Generator', url: '/tools/random-number-generator' }
+	];
 </script>
 
 <div
@@ -8,4 +17,12 @@
 	<h1 class="text-4xl">
 		EduTools {#if $preferencesStore.experimentalFeatures}Experimental{/if}
 	</h1>
+</div>
+
+<div class="flex w-full flex-row gap-3 p-3">
+	<div class="grid grow grid-cols-3 gap-3">
+		{#each tools as tool}
+			<a href={tool.url} class="bg-accent flex justify-center rounded p-3 py-10">{tool.name}</a>
+		{/each}
+	</div>
 </div>
