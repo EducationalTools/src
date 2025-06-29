50137c2b57dc4b76dac27b3078121c940e466a88
Ingo Wolf <me@ingowolf.au>
2025-06-29 10:26:34 +1000

    chnage gradient

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,7 +17,7 @@
 </script>
 
 <div
-	class="from-accent/50 flex w-full items-center justify-center bg-gradient-to-b to-transparent py-52"
+	class="from-primary/20 flex w-full items-center justify-center bg-gradient-to-b to-transparent py-52"
 >
 	<h1 class="text-4xl">
 		EduTools {#if $preferencesStore.experimentalFeatures}Experimental{/if}
