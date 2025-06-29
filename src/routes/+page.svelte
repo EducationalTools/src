456b543c2b2e1526571d18cdb3599d56099d0a46
Ingo Wolf <me@ingowolf.au>
2025-06-29 10:25:20 +1000

    add history and saved and change styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
-	import { preferencesStore } from '$lib/stores';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import { preferencesStore, historyStore, favoritesStore } from '$lib/stores';
+	import clsx from 'clsx';
+	import { getGameById } from '$lib/gmaes';
+	import History from '@lucide/svelte/icons/history';
+	import Bookmark from 'lucide-svelte/icons/bookmark';
 
 	const tools = [
 		{ name: 'Calculator', url: '/tools/calculator' },
@@ -19,10 +24,39 @@
 	</h1>
 </div>
 
-<div class="flex w-full flex-row gap-3 p-3">
-	<div class="grid grow grid-cols-3 gap-3">
+<div class="flex w-full flex-col gap-3 p-3 md:flex-row">
+	<div
+		class={clsx(
+			'grid h-fit w-full gap-3',
+			$preferencesStore.experimentalFeatures ? 'grid-cols-1' : 'grid-cols-3'
+		)}
+	>
 		{#each tools as tool}
-			<a href={tool.url} class="bg-accent flex justify-center rounded p-3 py-10">{tool.name}</a>
+			<Button variant="outline" class="py-10 text-xl" href={tool.url}>{tool.name}</Button>
 		{/each}
 	</div>
+	{#if $preferencesStore.experimentalFeatures}
+		<div class="grid h-fit w-full grid-cols-1 gap-3">
+			<div class="flex flex-row items-center gap-1">
+				<History />
+				History
+			</div>
+			{#each $historyStore as item}
+				<Button variant="outline" class="py-10 text-xl" href={'/g/' + item}
+					>{getGameById(item)?.name}</Button
+				>
+			{/each}
+		</div>
+		<div class="grid h-fit w-full grid-cols-1 gap-3">
+			<div class="flex flex-row items-center gap-1">
+				<Bookmark />
+				Saved
+			</div>
+			{#each $favoritesStore as item}
+				<Button variant="outline" class="py-10 text-xl" href={'/g/' + item}
+					>{getGameById(item)?.name}</Button
+				>
+			{/each}
+		</div>
+	{/if}
 </div>
