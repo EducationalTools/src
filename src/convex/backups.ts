9d3cfce742c9c253dd766c5d661b5c6b4e14ee68
Ingo Wolf <me@ingowolf.au>
2025-07-04 11:41:38 +1000

    add test backusp

--- src/convex/backups.ts
+++ src/convex/backups.ts
@@ -1,8 +1,15 @@
+import { v } from 'convex/values';
 import { query } from './_generated/server';
 
 export const get = query({
-	args: {},
-	handler: async (ctx) => {
-		return 'test';
+	args: {
+		id: v.string()
+	},
+	handler: async (ctx, args) => {
+		const backups = await ctx.db
+			.query('backup')
+			.filter((q) => q.eq(q.field('user'), args.id))
+			.take(100);
+		return backups.map((backup) => backup.name);
 	}
 });
---
+++ src/routes/backups/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import {
+		SignedIn,
+		SignedOut,
+		SignInButton,
+		UserButton,
+		useClerkContext,
+		SignIn,
+		SignUp,
+		UserProfile
+	} from 'svelte-clerk/client';
+	const ctx = useClerkContext();
+
+	let sessionToken = $state('');
+
+	$effect(() => {
+		ctx.session?.getToken().then((token) => {
+			if (token) {
+				sessionToken = token;
+			}
+		});
+	});
+
+	import { useQuery } from 'convex-svelte';
+	import { api } from '$lib/../convex/_generated/api.js';
+
+	let query = $state(useQuery(api.backups.get, { id: '' }));
+
+	$effect(() => {
+		query = useQuery(api.backups.get, { id: sessionToken });
+	});
+</script>
+
+<!-- <SignedIn>{sessionToken}</SignedIn>
+<SignedOut>You need account</SignedOut> -->
+
+{query.data}
