b260b91ffd19f290a21890d4c8e00447e4bfd459
Ingo Wolf <me@ingowolf.au>
2025-07-14 14:03:40 +1000

    delete space

--- src/routes/backups/+page.svelte
+++ src/routes/backups/+page.svelte
@@ -57,7 +57,7 @@
 				<Card.Header>
 					<Card.Title>{backup.name}</Card.Title>
 					<Card.Description>
-						{dayjs(backup.creationTime).format('HH:MM a  DD/MM/YY')}
+						{dayjs(backup.creationTime).format('HH:MMa  DD/MM/YY')}
 					</Card.Description>
 				</Card.Header>
 				<Card.Footer class="flex flex-row gap-3">
