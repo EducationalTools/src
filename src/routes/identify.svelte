3cc1f2445a8bc9ccfdd6b8176d6f794fc55979c6
Ingo Wolf <me@ingowolf.au>
2025-07-14 17:20:14 +1000

    add posthog reset and better error handlinga

--- src/routes/identify.svelte
+++ src/routes/identify.svelte
@@ -5,12 +5,14 @@
 	const userCtx = useClerkContext();
 
 	$effect(() => {
-		if (userCtx.user) {
+		if (userCtx.user && userCtx.user.emailAddresses?.length > 0) {
 			posthog.identify(userCtx.user.id, {
 				email: userCtx.user.emailAddresses[0].emailAddress,
 				name: userCtx.user.fullName,
 				clerkId: userCtx.user.id
 			});
+		} else {
+			posthog.reset();
 		}
 	});
 </script>
