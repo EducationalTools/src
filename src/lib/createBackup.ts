133cb379d4d990ddc275ed13caba51aef38b846e
Ingo Wolf <me@ingowolf.au>
2025-07-14 17:12:50 +1000

    add filtering to createbackup

--- src/lib/createBackup.ts
+++ src/lib/createBackup.ts
@@ -1,9 +1,40 @@
 export default function createBackup() {
+	let cookies = document.cookie.split('; ');
+	let filteredCookies = cookies.filter((cookie) => {
+		return !(
+			cookie.trim().startsWith('__') ||
+			cookie.trim().startsWith('clerk') ||
+			cookie.trim().startsWith('ph')
+		);
+	});
+	let cookieString = filteredCookies.join('; ');
+
+	let filteredLocalStorage = Object.entries(localStorage).filter(([key, value]) => {
+		return !(
+			key.startsWith('__') ||
+			key.startsWith('clerk') ||
+			key.startsWith('ph') ||
+			key == 'lastSeenSurveyDate'
+		);
+	});
+	let filteredLocalStorageObject = Object.fromEntries(filteredLocalStorage);
+	let filteredSessionStorage = Object.entries(sessionStorage).filter(([key, value]) => {
+		return !(
+			key.startsWith('__') ||
+			key.startsWith('clerk') ||
+			key.startsWith('ph') ||
+			key == 'lastSeenSurveyDate'
+		);
+	});
+	let filteredSessionStorageObject = Object.fromEntries(filteredSessionStorage);
+
 	let data = {
-		cookies: document.cookie,
-		localstorage: localStorage,
-		sessionstorage: sessionStorage
+		cookies: cookieString,
+		localstorage: filteredLocalStorageObject,
+		sessionstorage: filteredSessionStorageObject
 	};
+
+	console.log(data);
 
 	return btoa(JSON.stringify(data));
 }
