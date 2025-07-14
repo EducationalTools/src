55c59d684562a15ba1f0f8b288de04e1b2677fad
Ingo Wolf <me@ingowolf.au>
2025-07-14 13:58:09 +1000

    add backup create

--- src/convex/backups.ts
+++ src/convex/backups.ts
@@ -1,5 +1,5 @@
 import { v } from 'convex/values';
-import { query } from './_generated/server';
+import { mutation, query } from './_generated/server';
 import * as jose from 'jose';
 
 export const get = query({
@@ -27,5 +27,34 @@ export const get = query({
 			data: backup.data,
 			creationTime: backup._creationTime
 		}));
+	}
+});
+
+export const create = mutation({
+	args: {
+		jwt: v.string(),
+		name: v.string()
+	},
+	handler: async (ctx, args) => {
+		if (!process.env.CLERK_JWT_KEY) {
+			throw new Error('Missing CLERK_JWT_KEY environment variable');
+		}
+		const publicKey = await jose.importSPKI(process.env.CLERK_JWT_KEY, 'RS256');
+		if (!publicKey) {
+			throw new Error('Missing CLERK_JWT_KEY environment variable');
+		}
+		if (args.jwt.length === 0) {
+			throw new Error('Missing JWT');
+		}
+		const { payload, protectedHeader } = await jose.jwtVerify(args.jwt, publicKey, {});
+		if (!payload.sub) {
+			throw new Error('Invalid JWT');
+		}
+		const backup = await ctx.db.insert('backup', {
+			user: payload.sub,
+			name: args.name,
+			data: ''
+		});
+		return backup.id;
 	}
 });
