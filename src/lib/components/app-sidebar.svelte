2eea2a1faffdaeca146e1100ae0c8654a6a2ad5c
Ingo Wolf <me@ingowolf.au>
2025-07-16 12:35:21 +1000

    add sidebar toggle event why not

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -52,6 +52,7 @@
 	} from 'svelte-clerk/client';
 	import { dark } from '@clerk/themes';
 	import History from '@lucide/svelte/icons/history';
+	import posthog from 'posthog-js';
 	const ctx = useClerkContext();
 
 	// Props
@@ -379,6 +380,7 @@
 		<Sidebar.MenuItem>
 			<Sidebar.MenuButton
 				onclick={() => {
+					posthog.capture('sidebar_toggle', { state: sidebar.open });
 					sidebar.toggle();
 				}}
 			>
