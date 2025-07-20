309eddb6941064b6dbc6379e7b388fc5fff4fb4e
Ingo Wolf <me@ingowolf.au>
2025-07-20 15:53:29 +1000

    add signed out state

--- src/routes/bugreport/+page.svelte
+++ src/routes/bugreport/+page.svelte
@@ -6,10 +6,10 @@
 	import * as Table from '$lib/components/ui/table/index.js';
 	import { onMount } from 'svelte';
 	import posthog from 'posthog-js';
-	import Button from '$lib/components/ui/button/button.svelte';
+	import Button, { buttonVariants } from '$lib/components/ui/button/button.svelte';
 	import { useConvexClient } from 'convex-svelte';
 	import { api } from '../../convex/_generated/api';
-	import { useClerkContext } from 'svelte-clerk';
+	import { SignedIn, SignedOut, SignInButton, SignUpButton, useClerkContext } from 'svelte-clerk';
 	import { toast } from 'svelte-sonner';
 
 	const id = $props.id();
@@ -57,71 +57,91 @@
 
 <div class="mx-auto flex w-full max-w-3xl flex-col gap-3 p-3">
 	<h1 class="text-3xl">Bug Report</h1>
-	<div class="flex w-full flex-col gap-1.5">
-		<Label for="brief-description-{id}">Brief Description</Label>
-		<Input type="text" id="brief-description-{id}" bind:value={briefDescription} />
-	</div>
-	<div class="flex w-full flex-col gap-1.5">
-		<Label for="description-{id}">Describe the bug</Label>
-		<p class="text-muted-foreground text-sm">A clear and concise description of what the bug is.</p>
-		<Textarea id="description-{id}" bind:value={description}></Textarea>
-	</div>
-	<div class="flex w-full flex-col gap-1.5">
-		<Label for="reproduction-{id}">How could you reproduce this issue?</Label>
-		<p class="text-muted-foreground text-sm">
-			Please provide a step-by-step guide on how to reproduce this issue.
-		</p>
-		<Textarea id="reproduction-{id}" bind:value={reproduction}></Textarea>
-	</div>
-	<div class="flex w-full flex-col gap-1.5">
-		<Label for="expected-{id}">What did you expect to happen?</Label>
-		<p class="text-muted-foreground text-sm">
-			A clear and concise description of what you expected to happen.
-		</p>
-		<Textarea id="expected-{id}" bind:value={expected}></Textarea>
-	</div>
-	<div class="flex w-full flex-col gap-1.5">
-		<Label for="log-{id}">Relevant log output</Label>
-		<p class="text-muted-foreground text-sm">Please copy and paste any relevant log output.</p>
-		<Textarea id="log-{id}" bind:value={log}></Textarea>
-	</div>
-	<div class="flex w-full flex-col gap-1.5">
-		<Label for="additional-{id}">Additional information</Label>
-		<Textarea id="additional-{id}" bind:value={additional}></Textarea>
-	</div>
-	<Accordion.Root type="single">
-		<Accordion.Item value="other-data">
-			<Accordion.Trigger>Other Data</Accordion.Trigger>
-			<Accordion.Content>
-				<Table.Root>
-					<Table.Body>
-						<Table.Row>
-							<Table.Cell>Distinct ID</Table.Cell>
-							<Table.Cell>{distinctId}</Table.Cell>
-						</Table.Row>
-						<Table.Row>
-							<Table.Cell>User agent</Table.Cell>
-							<Table.Cell>{userAgent}</Table.Cell>
-						</Table.Row>
-					</Table.Body>
-				</Table.Root>
-			</Accordion.Content>
-		</Accordion.Item>
-	</Accordion.Root>
-	<Button
-		variant="outline"
-		onclick={() => {
-			client.mutation(api.issues.bugReport, {
-				additional,
-				briefDescription,
-				description,
-				distinctId,
-				expected,
-				log,
-				reproduction,
-				userAgent,
-				jwt: sessionToken
-			});
-		}}>Submit</Button
-	>
+	<SignedOut>
+		<div class="flex flex-row items-center justify-center gap-3">
+			<SignUpButton class={buttonVariants({ variant: 'default' })} />
+			or
+			<Button
+				variant="outline"
+				target="_blank"
+				href="https://github.com/EducationalTools/src/issues/new?template=bug_report.yml"
+				>Report on Github</Button
+			>
+		</div>
+		<div class="flex w-full flex-row items-center justify-center gap-3">
+			Already have an account?
+			<SignInButton class={buttonVariants({ variant: 'ghost' })} />
+		</div>
+	</SignedOut>
+	<SignedIn>
+		<div class="flex w-full flex-col gap-1.5">
+			<Label for="brief-description-{id}">Brief Description</Label>
+			<Input type="text" id="brief-description-{id}" bind:value={briefDescription} />
+		</div>
+		<div class="flex w-full flex-col gap-1.5">
+			<Label for="description-{id}">Describe the bug</Label>
+			<p class="text-muted-foreground text-sm">
+				A clear and concise description of what the bug is.
+			</p>
+			<Textarea id="description-{id}" bind:value={description}></Textarea>
+		</div>
+		<div class="flex w-full flex-col gap-1.5">
+			<Label for="reproduction-{id}">How could you reproduce this issue?</Label>
+			<p class="text-muted-foreground text-sm">
+				Please provide a step-by-step guide on how to reproduce this issue.
+			</p>
+			<Textarea id="reproduction-{id}" bind:value={reproduction}></Textarea>
+		</div>
+		<div class="flex w-full flex-col gap-1.5">
+			<Label for="expected-{id}">What did you expect to happen?</Label>
+			<p class="text-muted-foreground text-sm">
+				A clear and concise description of what you expected to happen.
+			</p>
+			<Textarea id="expected-{id}" bind:value={expected}></Textarea>
+		</div>
+		<div class="flex w-full flex-col gap-1.5">
+			<Label for="log-{id}">Relevant log output</Label>
+			<p class="text-muted-foreground text-sm">Please copy and paste any relevant log output.</p>
+			<Textarea id="log-{id}" bind:value={log}></Textarea>
+		</div>
+		<div class="flex w-full flex-col gap-1.5">
+			<Label for="additional-{id}">Additional information</Label>
+			<Textarea id="additional-{id}" bind:value={additional}></Textarea>
+		</div>
+		<Accordion.Root type="single">
+			<Accordion.Item value="other-data">
+				<Accordion.Trigger>Other Data</Accordion.Trigger>
+				<Accordion.Content>
+					<Table.Root>
+						<Table.Body>
+							<Table.Row>
+								<Table.Cell>Distinct ID</Table.Cell>
+								<Table.Cell>{distinctId}</Table.Cell>
+							</Table.Row>
+							<Table.Row>
+								<Table.Cell>User agent</Table.Cell>
+								<Table.Cell>{userAgent}</Table.Cell>
+							</Table.Row>
+						</Table.Body>
+					</Table.Root>
+				</Accordion.Content>
+			</Accordion.Item>
+		</Accordion.Root>
+		<Button
+			variant="outline"
+			onclick={() => {
+				client.mutation(api.issues.bugReport, {
+					additional,
+					briefDescription,
+					description,
+					distinctId,
+					expected,
+					log,
+					reproduction,
+					userAgent,
+					jwt: sessionToken
+				});
+			}}>Submit</Button
+		>
+	</SignedIn>
 </div>
