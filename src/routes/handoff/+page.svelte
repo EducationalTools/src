2861654f306e44d59e03589df87424bdff0c0a9e
Ingo Wolf <me@ingowolf.au>
2025-07-22 13:39:01 +1000

    add content

--- src/routes/handoff/+page.svelte
+++ src/routes/handoff/+page.svelte
@@ -1,2 +1,34 @@
 <script lang="ts">
+	import { page } from '$app/state';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import * as Card from '$lib/components/ui/card/index.js';
+	import restoreBackup from '$lib/restoreBackup';
 </script>
+
+<div class="flex h-full w-full items-center justify-center p-3">
+	<Card.Root>
+		<Card.Content>
+			<Card.Title>Importing data</Card.Title>
+			<Card.Description>
+				This will clear all of your existing data on <pre>{page.url.hostname}</pre>
+			</Card.Description>
+		</Card.Content>
+		<Card.Footer class="flex flex-row justify-end gap-3">
+			<Button
+				variant="outline"
+				onclick={() => {
+					history.back();
+				}}>Cancel</Button
+			>
+			<Button
+				variant="destructive"
+				onclick={() => {
+					let data = page.url.searchParams.get('data');
+					if (data) {
+						restoreBackup(data);
+					}
+				}}>Continue</Button
+			>
+		</Card.Footer>
+	</Card.Root>
+</div>
