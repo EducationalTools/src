48afbcde08f2dca06a84a8cf17beac3f85d674a5
Ingo Wolf <me@ingowolf.au>
2025-07-24 06:57:33 +1000

    add exp features tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -15,3 +15,17 @@ test('menu before experimental features', async ({ page }) => {
 	await expect(page.getByText('Gmaes', { exact: true })).not.toBeVisible();
 	await expect(page).toHaveTitle(/EduTools/);
 });
+
+test('menu after experimental features', async ({ page }) => {
+	await page.goto('http://localhost:4173/');
+
+	await page.getByRole('button', { name: 'Search ⌘ K' }).click();
+	await page.getByPlaceholder('Type a command or search...').fill('experimental');
+	await page.getByRole('option', { name: 'Toggle experimental features' }).click();
+	await page.locator('#bits-c7').press('Escape');
+
+	await expect(page.locator('a').getByText('Home', { exact: true })).toBeVisible();
+	await expect(page.locator('a').getByText('Tools', { exact: true })).toBeVisible();
+	await expect(page.locator('a').getByText('Gmaes', { exact: true })).toBeVisible();
+	await expect(page).toHaveTitle(/EduTools/);
+});
