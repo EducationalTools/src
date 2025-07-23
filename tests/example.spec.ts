a0b44d37704d891aa2a8e83efa3abc492ed3f2c8
Ingo Wolf <me@ingowolf.au>
2025-07-24 06:51:32 +1000

    Add tests for menu visibility and page title

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,7 +2,15 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
 	await page.goto('http://localhost:4173/');
-	await expect(page.locator('h1')).toContainText('EduTools');
 
+	await expect(page.locator('h1')).toContainText('EduTools');
+	await expect(page).toHaveTitle(/EduTools/);
+});
+
+test('menu before experimental features', async ({ page }) => {
+	await page.goto('http://localhost:4173/');
+
+	await expect(page.getByText('Tools', { exact: true })).toBeVisible();
+	await expect(page.getByText('Home', { exact: true })).toBeVisible();
 	await expect(page).toHaveTitle(/EduTools/);
 });
