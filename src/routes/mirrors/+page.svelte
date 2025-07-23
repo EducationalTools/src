82ed1ca0b58eed8ab987be62d9c2b89ef38b9fbb
Ingo Wolf <me@ingowolf.au>
2025-07-23 18:22:19 +1000

    Add more mirrors and improve quality badges on mirrors page

--- src/routes/mirrors/+page.svelte
+++ src/routes/mirrors/+page.svelte
@@ -4,10 +4,23 @@
 	import * as Card from '$lib/components/ui/card/index.js';
 	import createBackup from '$lib/createBackup';
 	import Trophy from '@lucide/svelte/icons/trophy';
+	import Star from '@lucide/svelte/icons/star';
+	import TriangleAlert from '@lucide/svelte/icons/triangle-alert';
 
-	let mirrors = [
-		{ url: 'https://edutools.ingo.au', notes: '', quality: 'highlyrecommended' },
-		{ url: 'https://edutools.ingowolf.au', notes: '', quality: 'recommended' }
+	interface Mirror {
+		url: string;
+		notes?: string;
+		quality?: string;
+	}
+
+	let mirrors: Mirror[] = [
+		{ url: 'https://edutools.ingo.au', quality: 'highlyrecommended' },
+		{ url: 'https://edutools.ingowolf.au', quality: 'recommended' },
+		{ url: 'https://educationaltools.github.io', quality: 'recommended' },
+		{ url: 'https://educationaltools.vercel.app', notes: '' },
+		{ url: 'https://edutools-d915e.web.app', notes: '' },
+		{ url: 'https://edutools-d915e.firebaseapp.com', notes: '' },
+		{ url: 'https://edutools.infinityfreeapp.com', quality: 'notrecommended' }
 	];
 </script>
 
@@ -17,12 +30,19 @@
 			<Card.Root>
 				<Card.Header>
 					<Card.Title>{new URL(mirror.url).hostname}</Card.Title>
+
 					{#if mirror.quality == 'highlyrecommended'}
 						<Badge class="bg-gradient-to-r from-yellow-300 to-yellow-500"
 							><Trophy />Highly Recommended</Badge
 						>
 					{:else if mirror.quality == 'recommended'}
-						<Badge class="bg-yellow-600 dark:bg-yellow-200"><Trophy />Recommended</Badge>
+						<Badge class="bg-yellow-600 dark:bg-yellow-200"><Star />Recommended</Badge>
+					{:else if mirror.quality == 'notrecommended'}
+						<Badge class="bg-red-600 dark:bg-red-200"><TriangleAlert />Not Recommended</Badge>
+					{/if}
+
+					{#if mirror.notes}
+						<Card.Description>{mirror.notes}</Card.Description>
 					{/if}
 				</Card.Header>
 				<Card.Footer class="flex flex-row gap-3">
