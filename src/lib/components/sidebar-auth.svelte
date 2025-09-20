1302f57fee493bd869304b3313d8c70cef813640
Ingo Wolf <me@ingowolf.au>
2025-09-20 17:15:33 +1000

    add sync state to sidebar

--- src/lib/components/sidebar-auth.svelte
+++ src/lib/components/sidebar-auth.svelte
@@ -41,6 +41,7 @@
 	import Switch from './ui/switch/switch.svelte';
 	import { syncSettingsStore } from '$lib/stores';
 	import clsx from 'clsx';
+	import { syncState } from '$lib/state.svelte';
 </script>
 
 {#if page.url.hostname == 'edutools.ingo.au' || page.url.hostname == 'localhost'}
@@ -63,7 +64,13 @@
 							<div
 								class="bg-muted text-muted-foreground pointer-events-none inline-flex h-5 items-center gap-1 rounded border px-1.5 font-mono text-[10px] font-medium opacity-100 select-none"
 							>
-								{$syncSettingsStore.enabled}
+								{$syncSettingsStore.enabled
+									? syncState.current == 'uploading'
+										? 'Uploading'
+										: syncState.current == 'downloading'
+											? 'Downloading'
+											: 'Synced'
+									: 'Disabled'}
 							</div>
 						</Sidebar.MenuButton>
 					{/snippet}
