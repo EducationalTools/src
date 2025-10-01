e57840a509045f6678758202d7d3a687fb1e5c1e
Ingo Wolf <me@ingowolf.au>
2025-10-01 19:12:03 +1000

    change base url to convex deployment

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,7 @@
 import { createAuthClient } from 'better-auth/svelte';
-import { convexClient } from "@convex-dev/better-auth/client/plugins";
+import { convexClient } from '@convex-dev/better-auth/client/plugins';
 
 export const authClient = createAuthClient({
-  plugins: [convexClient()],
+	plugins: [convexClient()],
+	baseURL: 'https://rightful-dogfish-709.convex.site'
 });
