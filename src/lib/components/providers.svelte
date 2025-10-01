5c868ecb13b522b6971925e0291305689d34f351
Ingo Wolf <me@ingowolf.au>
2025-10-01 19:09:28 +1000

    add convex provider

--- src/lib/components/providers.svelte
+++ src/lib/components/providers.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
 	import { ModeWatcher } from 'mode-watcher';
-	// import { setupConvex } from 'convex-svelte';
+	import { setupConvex } from 'convex-svelte';
 
 	// Props
 	let { children } = $props();
 
 	// Setup Convex
 	// Only setup Convex if the URL is provided
-	// if (process.env.PUBLIC_CONVEX_URL) {
-	// 	try {
-	// 		setupConvex(process.env.PUBLIC_CONVEX_URL);
-	// 	} catch (error) {
-	// 		console.warn('Failed to setup Convex:', error);
-	// 	}
-	// } else {
-	// 	console.log('Convex setup skipped: PUBLIC_CONVEX_URL not configured (likely dev build)');
-	// }
+	if (process.env.PUBLIC_CONVEX_URL) {
+		try {
+			setupConvex(process.env.PUBLIC_CONVEX_URL);
+		} catch (error) {
+			console.warn('Failed to setup Convex:', error);
+		}
+	} else {
+		console.log('Convex setup skipped: PUBLIC_CONVEX_URL not configured (likely dev build)');
+	}
 </script>
 
 <ModeWatcher disableTransitions={false} defaultMode={'dark'} />
