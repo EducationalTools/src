8146719deb27ce7ac398df905283b91bc2a31565
Ingo Wolf <me@ingowolf.au>
2025-10-06 15:17:20 +1100

    add redirect thing

--- src/convex/http.ts
+++ src/convex/http.ts
@@ -1,8 +1,23 @@
 import { httpRouter } from 'convex/server';
 import { authComponent, createAuth } from './auth';
+import { httpAction } from './_generated/server';
 
 const http = httpRouter();
 
 authComponent.registerRoutes(http, createAuth, { cors: { allowedOrigins: ['*'] } });
+
+http.route({
+	path: '/',
+	method: 'GET',
+	handler: httpAction(async (ctx, request) => {
+		const referer = request.headers.get('Referer');
+		return new Response(null, {
+			status: 302,
+			headers: new Headers({
+				Location: referer || 'https://edutools.ingo.au'
+			})
+		});
+	})
+});
 
 export default http;
