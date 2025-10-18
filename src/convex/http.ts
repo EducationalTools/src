08bda5622a5c94b9c2dd25d619770a38df97a8ab
Ingo Wolf <me@ingowolf.au>
2025-10-18 15:21:26 +1100

    add http route for /auth

--- src/convex/http.ts
+++ src/convex/http.ts
@@ -6,4 +6,14 @@ const http = httpRouter();
 
 authComponent.registerRoutes(http, createAuth, { cors: { allowedOrigins: ['*'] } });
 
+http.route({
+	method: 'GET',
+	path: '/auth',
+	handler: httpAction(async (ctx, request) => {
+		return new Response(JSON.stringify({ message: 'Hello, world!' }), {
+			headers: { 'Content-Type': 'application/json' }
+		});
+	})
+});
+
 export default http;
