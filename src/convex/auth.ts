251ff49a09d9aa956e9c62de6f46024da91082c1
Ingo Wolf <me@ingowolf.au>
2025-10-18 17:47:39 +1100

    add OTT plugin

--- src/convex/auth.ts
+++ src/convex/auth.ts
@@ -4,7 +4,7 @@ import { components } from './_generated/api';
 import { type DataModel } from './_generated/dataModel';
 import { query } from './_generated/server';
 import { betterAuth } from 'better-auth';
-import { createAuthMiddleware, oAuthProxy } from 'better-auth/plugins';
+import { createAuthMiddleware, oAuthProxy, oneTimeToken } from 'better-auth/plugins';
 
 const siteUrl = process.env.PUBLIC_CONVEX_SITE_URL!; // redirects to the convex deployment, which redirects to the referer. if it works don't touch it
 
@@ -40,6 +40,8 @@ export const createAuth = (
 		plugins: [
 			// The Convex plugin is required for Convex compatibility
 			convex(),
+
+			oneTimeToken(),
 
 			// Disable state check
 			// refer to comment on line 25
--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,8 @@
 import { createAuthClient } from 'better-auth/svelte';
 import { convexClient, crossDomainClient } from '@convex-dev/better-auth/client/plugins';
+import { oneTimeTokenClient } from 'better-auth/client/plugins';
 
 export const authClient = createAuthClient({
-	plugins: [convexClient()],
+	plugins: [convexClient(), oneTimeTokenClient()],
 	baseURL: 'https://rightful-dogfish-709.convex.site'
 });
