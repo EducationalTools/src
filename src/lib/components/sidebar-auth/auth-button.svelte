281b55751e6aaa002ed0b2965f8da263f89ebb8d
Ingo Wolf <me@ingowolf.au>
2025-10-20 12:17:26 +1100

    add loader

--- src/lib/components/sidebar-auth/auth-button.svelte
+++ src/lib/components/sidebar-auth/auth-button.svelte
@@ -1,17 +1,25 @@
 <script lang="ts">
 	import { authClient } from '$lib/auth-client';
+	import { LoaderCircle } from '@lucide/svelte';
 	import Button from '../ui/button/button.svelte';
 
 	const { provider, children } = $props();
+
+	let loading = $state(false);
 </script>
 
 <Button
+	disabled={loading}
 	onclick={() => {
+		loading = true;
 		authClient.signIn.social({
 			provider: provider,
 			callbackURL: process.env.PUBLIC_CONVEX_SITE_URL + '/auth?redirect=' + window.location.href
 		});
 	}}
 >
+	{#if loading}
+		<LoaderCircle class="animate-spin" />
+	{/if}
 	{@render children()}
 </Button>
