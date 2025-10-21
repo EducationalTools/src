92f54f5788702f88d610fb2e9a361931537d8f8f
Ingo Wolf <me@ingowolf.au>
2025-10-21 16:18:15 +1100

    add get query

--- src/convex/profiles.ts
+++ src/convex/profiles.ts
@@ -1,3 +1,4 @@
+import { v } from 'convex/values';
 import { query } from './_generated/server';
 import { authComponent } from './auth';
 
@@ -23,6 +24,40 @@ export const getCurrent = query({
 
 		return {
 			picture: pictureUrl,
+			name: currentUser.name,
+			...profile
+		};
+	}
+});
+
+export const get = query({
+	args: { userId: v.string() },
+	handler: async (ctx, { userId }) => {
+		const profile = await ctx.db
+			.query('profiles')
+			.withIndex('by_user', (q) => q.eq('userId', userId))
+			.first();
+
+		const user = await authComponent.getAnyUserById(ctx, userId);
+
+		if (!user) {
+			throw new Error('User not found');
+		}
+
+		if (!profile) {
+			throw new Error('Profile not found');
+		}
+
+		let pictureUrl;
+		if (profile.picture) {
+			pictureUrl = await ctx.storage.getUrl(profile.picture);
+		} else {
+			pictureUrl = null;
+		}
+
+		return {
+			picture: pictureUrl,
+			name: user.name,
 			...profile
 		};
 	}
