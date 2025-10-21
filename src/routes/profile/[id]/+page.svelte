b76baf4b30713849f6c54fd7982ffc051d760c8e
Ingo Wolf <me@ingowolf.au>
2025-10-21 16:44:16 +1100

    add profile page for any user

---
+++ src/routes/profile/[id]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { page } from '$app/state';
+
+	import { api } from '$convex/_generated/api';
+	import ProfilePage from '../profile-page.svelte';
+
+	import { useQuery } from 'convex-svelte';
+
+	const profile = useQuery(api.profiles.get, { userId: page.params.id || '' });
+</script>
+
+<ProfilePage
+	bio={profile?.data?.bio}
+	picture={profile?.data?.picture}
+	name={profile?.data?.name}
+	pronouns={profile?.data?.pronouns}
+	loading={profile?.isLoading ?? false}
+/>
