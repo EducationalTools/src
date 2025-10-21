382b867e04afbbd2fd36dca1b3925d5f18d9c39c
Ingo Wolf <me@ingowolf.au>
2025-10-21 13:44:59 +1100

    add trusted mirrrors query

---
+++ src/convex/trustedmirrors.ts
@@ -0,0 +1,16 @@
+import { v } from 'convex/values';
+import { internalQuery } from './_generated/server';
+
+export const isTrusted = internalQuery({
+	args: {
+		host: v.string()
+	},
+	handler: async (ctx, args) => {
+		const entry = ctx.db
+			.query('trustedMirrors')
+			.withIndex('by_host', (q) => q.eq('host', args.host))
+			.first();
+
+		return !!entry;
+	}
+});
