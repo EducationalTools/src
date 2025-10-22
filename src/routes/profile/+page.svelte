5b70357a5387fff849453dba0cf59e92e88334fb
Ingo Wolf <me@ingowolf.au>
2025-10-22 17:54:50 +1100

    use a more general profile component

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { api } from '$convex/_generated/api';
-	import ProfilePage from '$lib/components/profile-page.svelte';
+	import Profile from '$lib/components/profile.svelte';
 
 	import { useQuery } from 'convex-svelte';
 
@@ -8,11 +8,15 @@
 	const user = useQuery(api.auth.getCurrentUser, {});
 </script>
 
-<ProfilePage
-	id={user.data?._id}
-	bio={profile?.data?.bio}
-	picture={profile?.data?.picture}
-	name={profile?.data?.name}
-	pronouns={profile?.data?.pronouns}
-	loading={profile?.isLoading ?? false}
-/>
+<div class="mx-auto w-full max-w-2xl p-4">
+	<div class="bg-card rounded-xl border p-4">
+		<Profile
+			id={user.data?._id}
+			bio={profile?.data?.bio}
+			picture={profile?.data?.picture}
+			name={profile?.data?.name}
+			pronouns={profile?.data?.pronouns}
+			loading={profile?.isLoading ?? false}
+		/>
+	</div>
+</div>
--- src/routes/profile/[id]/+page.svelte
+++ src/routes/profile/[id]/+page.svelte
@@ -2,18 +2,22 @@
 	import { page } from '$app/state';
 
 	import { api } from '$convex/_generated/api';
-	import ProfilePage from '$lib/components/profile-page.svelte';
+	import Profile from '$lib/components/profile.svelte';
 
 	import { useQuery } from 'convex-svelte';
 
 	const profile = useQuery(api.profiles.get, { userId: page.params.id || '' });
 </script>
 
-<ProfilePage
-	id={page.params.id}
-	bio={profile?.data?.bio}
-	picture={profile?.data?.picture}
-	name={profile?.data?.name}
-	pronouns={profile?.data?.pronouns}
-	loading={profile?.isLoading ?? false}
-/>
+<div class="mx-auto w-full max-w-2xl p-4">
+	<div class="bg-card rounded-xl border p-4">
+		<Profile
+			id={page.params.id}
+			bio={profile?.data?.bio}
+			picture={profile?.data?.picture}
+			name={profile?.data?.name}
+			pronouns={profile?.data?.pronouns}
+			loading={profile?.isLoading ?? false}
+		/>
+	</div>
+</div>
