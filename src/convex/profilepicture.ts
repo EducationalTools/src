813e2a0a25275434efd724d7a13efb9f56e36dbd
Ingo Wolf <me@ingowolf.au>
2025-10-25 08:46:51 +1100

    add clenaup

--- src/convex/profilepicture.ts
+++ src/convex/profilepicture.ts
@@ -1,5 +1,5 @@
 import { v } from 'convex/values';
-import { internalAction, internalMutation } from './_generated/server';
+import { internalAction, internalMutation, internalQuery } from './_generated/server';
 import { Id } from './_generated/dataModel';
 import { internal } from './_generated/api';
 
@@ -11,6 +11,11 @@ export const setFromUrl = internalAction({
 	handler: async (ctx, args) => {
 		const response = await fetch(args.url);
 		const image = await response.blob();
+		const profile = await ctx.runQuery(internal.profilepicture.internalGetUserPicture, {
+			userId: args.userId
+		});
+
+		if (!profile) return;
 
 		const storageId: Id<'_storage'> = await ctx.storage.store(image);
 
@@ -18,6 +23,28 @@ export const setFromUrl = internalAction({
 			storageId,
 			userId: args.userId
 		});
+
+		if (profile.picture) {
+			await ctx.storage.delete(profile.picture);
+		}
+	}
+});
+
+export const internalGetUserPicture = internalQuery({
+	args: {
+		userId: v.string()
+	},
+	handler: async (ctx, args) => {
+		const profile = await ctx.db
+			.query('profiles')
+			.withIndex('by_user', (q) => q.eq('userId', args.userId))
+			.first();
+
+		if (!profile) return null;
+
+		return {
+			picture: profile.picture
+		};
 	}
 });
 
