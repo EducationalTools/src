5bbc2288205a126b3a9182f691f559d3d5d489f3
Ingo Wolf <me@ingowolf.au>
2025-11-03 16:31:16 +1100

    delete everything

--- src/routes/g/[id]/+page.ts
+++
@@ -1,7 +0,0 @@
-import { error } from '@sveltejs/kit';
-import { getGameById } from '$lib/gmaes';
-
-export function load({ url }) {
-	let gmae = getGameById(url.pathname.split('/').slice(2).join('/'));
-	if (!gmae) throw error(404, 'Not Found');
-}
--- src/routes/tools/simple-text-editor/+page.ts
+++
@@ -1,5 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-
-export function load() {
-	redirect(302, '/tools/rich-text-editor'); // needs `throw` in v1
-}
