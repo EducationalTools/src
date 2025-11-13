ef6d9c8386079456b2d86c7694d8e6b7b8bfe39c
Ingo Wolf <me@ingowolf.au>
2025-11-13 22:24:49 +1100

    add button actions

--- app/routes/g/play.tsx
+++ app/routes/g/play.tsx
@@ -19,6 +19,8 @@ import {
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { useRef } from "react";
+import { toast } from "sonner";
 
 export async function clientLoader({ params }: Route.ActionArgs) {
   if (!params.id) throw data(null, { status: 404 });
@@ -27,9 +29,15 @@ export async function clientLoader({ params }: Route.ActionArgs) {
 
 export default function Play({ params }: Route.ComponentProps) {
   const gmae = getGmaeById(params.id);
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+
   return (
     <div className="w-full h-full flex flex-col p-2 md:pl-0 gap-2">
-      <iframe src={gmae?.url} className="w-full h-full rounded-lg"></iframe>
+      <iframe
+        src={gmae?.url}
+        ref={iframeRef}
+        className="w-full h-full rounded-lg"
+      ></iframe>
       <div className="flex flex-col md:flex-row gap-2 bg-card border rounded-lg p-2">
         <div className="flex flex-col p-2">
           <p className="text-sm text-muted-foreground">{gmae?.category}</p>
@@ -43,7 +51,14 @@ export default function Play({ params }: Route.ComponentProps) {
           <ButtonGroup>
             <Tooltip>
               <TooltipTrigger asChild>
-                <Button size="icon" variant="outline">
+                <Button
+                  size="icon"
+                  variant="outline"
+                  onClick={() => {
+                    iframeRef.current?.focus();
+                    toast.success("Focused gmae");
+                  }}
+                >
                   <Keyboard />
                   <span className="sr-only">Fix keyboard input</span>
                 </Button>
@@ -52,7 +67,13 @@ export default function Play({ params }: Route.ComponentProps) {
             </Tooltip>
             <Tooltip>
               <TooltipTrigger asChild>
-                <Button size="icon" variant="outline">
+                <Button
+                  size="icon"
+                  variant="outline"
+                  onClick={() => {
+                    iframeRef.current?.contentWindow?.location.reload();
+                  }}
+                >
                   <RefreshCw />
                   <span className="sr-only">Reload</span>
                 </Button>
@@ -61,7 +82,24 @@ export default function Play({ params }: Route.ComponentProps) {
             </Tooltip>
             <Tooltip>
               <TooltipTrigger asChild>
-                <Button size="icon" variant="outline">
+                <Button
+                  size="icon"
+                  variant="outline"
+                  onClick={() => {
+                    const win = window.open("");
+
+                    let frame = document.createElement("iframe");
+                    frame.src = gmae?.url || "";
+                    frame.style.width = "100%";
+                    frame.style.height = "100%";
+                    frame.style.border = "none";
+                    win?.document.body.appendChild(frame);
+                    if (win) {
+                      win.document.body.style.margin = "0";
+                      win.document.title = "Google";
+                    }
+                  }}
+                >
                   <ExternalLink />
                   <span className="sr-only">Open in new tab</span>
                 </Button>
@@ -70,7 +108,13 @@ export default function Play({ params }: Route.ComponentProps) {
             </Tooltip>
             <Tooltip>
               <TooltipTrigger asChild>
-                <Button size="icon">
+                <Button
+                  size="icon"
+                  onClick={() => {
+                    iframeRef.current?.requestFullscreen();
+                    iframeRef.current?.focus();
+                  }}
+                >
                   <Maximize />
                   <span className="sr-only">Fullscreen</span>
                 </Button>
