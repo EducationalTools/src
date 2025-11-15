6c2cf585b57bfb10d8012eb8055ada0f19b6fe4b
Ingo Wolf <me@ingowolf.au>
2025-11-15 14:10:05 +1100

    add nice animation

--- components/header.tsx
+++ components/header.tsx
@@ -2,30 +2,42 @@ import { Link } from "react-router";
 import { SidebarTrigger, useSidebar } from "./ui/sidebar";
 import { Button } from "./ui/button";
 import { Sidebar } from "lucide-react";
+import { motion, AnimatePresence } from "motion/react";
 
 export default function Header() {
   const sidebar = useSidebar();
 
   return (
     <div className="flex flex-row gap-1 items-center px-2">
-      {!sidebar.open && (
-        <Button variant="ghost" size="icon" asChild>
-          <Link to="/">
-            <img
-              src="/edutools-black.svg"
-              className="dark:invert size-6"
-              alt=""
-            />
-          </Link>
-        </Button>
-      )}
-      <Button
-        variant="ghost"
-        size="icon"
-        onClick={() => sidebar.toggleSidebar()}
-      >
-        <Sidebar />
-      </Button>
+      <AnimatePresence mode="popLayout">
+        {!sidebar.open && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            key="header-logo"
+          >
+            <Button variant="ghost" size="icon" asChild>
+              <Link to="/">
+                <img
+                  src="/edutools-black.svg"
+                  className="dark:invert size-6"
+                  alt=""
+                />
+              </Link>
+            </Button>
+          </motion.div>
+        )}
+        <motion.div layout key="header-sidebar" transition={{ duration: 0.2 }}>
+          <Button
+            variant="ghost"
+            size="icon"
+            onClick={() => sidebar.toggleSidebar()}
+          >
+            <Sidebar />
+          </Button>
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 }
