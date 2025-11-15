4dd1ed9649b3e9a025adfe6dbbb47b53ece60cfb
Ingo Wolf <me@ingowolf.au>
2025-11-15 15:44:54 +1100

    fix conditional logic

--- components/header.tsx
+++ components/header.tsx
@@ -21,34 +21,33 @@ export default function Header() {
   return (
     <div className="flex flex-row gap-1 items-center">
       <AnimatePresence mode="popLayout">
-        {!sidebar.open ||
-          (sidebar.isMobile && (
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.2 }}
-              key="header-sidebar-collapsed-buttons"
-              className="flex flex-row gap-2"
+        {(!sidebar.open || sidebar.isMobile) && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.2 }}
+            key="header-sidebar-collapsed-buttons"
+            className="flex flex-row gap-2"
+          >
+            <Button variant="ghost" size="icon" asChild>
+              <Link to="/">
+                <img
+                  src="/edutools-black.svg"
+                  className="dark:invert size-6"
+                  alt=""
+                />
+              </Link>
+            </Button>
+            <Button
+              variant="ghost"
+              size="icon"
+              onClick={() => setSearchOpen(true)}
             >
-              <Button variant="ghost" size="icon" asChild>
-                <Link to="/">
-                  <img
-                    src="/edutools-black.svg"
-                    className="dark:invert size-6"
-                    alt=""
-                  />
-                </Link>
-              </Button>
-              <Button
-                variant="ghost"
-                size="icon"
-                onClick={() => setSearchOpen(true)}
-              >
-                <Search />
-              </Button>
-            </motion.div>
-          ))}
+              <Search />
+            </Button>
+          </motion.div>
+        )}
         <motion.div layout key="header-sidebar" transition={{ duration: 0.2 }}>
           <Button
             variant="ghost"
