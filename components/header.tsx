e8b8d9bfcf4a3bf9356c977e5144b7aeb856e56a
Ingo Wolf <me@ingowolf.au>
2025-11-15 14:01:57 +1100

    change some sidebar stuff

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,31 @@
-import { SidebarTrigger } from "./ui/sidebar";
+import { Link } from "react-router";
+import { SidebarTrigger, useSidebar } from "./ui/sidebar";
+import { Button } from "./ui/button";
+import { Sidebar } from "lucide-react";
 
 export default function Header() {
+  const sidebar = useSidebar();
+
   return (
-    <div className="flex flex-row gap-2">
-      <SidebarTrigger />
+    <div className="flex flex-row gap-1 items-center px-2">
+      {!sidebar.open && (
+        <Button variant="ghost" size="icon" asChild>
+          <Link to="/">
+            <img
+              src="/edutools-black.svg"
+              className="dark:invert size-6"
+              alt=""
+            />
+          </Link>
+        </Button>
+      )}
+      <Button
+        variant="ghost"
+        size="icon"
+        onClick={() => sidebar.toggleSidebar()}
+      >
+        <Sidebar />
+      </Button>
     </div>
   );
 }
