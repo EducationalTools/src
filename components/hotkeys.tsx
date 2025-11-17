157bf6867dc6c0bef2eaac830354b1250ffe5c8d
Ingo Wolf <me@ingowolf.au>
2025-11-17 12:19:50 +1100

    add keybind for home

--- components/hotkeys.tsx
+++ components/hotkeys.tsx
@@ -1,14 +1,19 @@
 import { useUiState } from "@/lib/state";
 import { useHotkeys } from "react-hotkeys-hook";
+import { useNavigate } from "react-router";
 
 export default function Hotkeys() {
   const setSearchOpen = useUiState((state) => state.setSearchOpen);
   const setSettingsOpen = useUiState((state) => state.setSettingsOpen);
+  const navigate = useNavigate();
 
   useHotkeys("mod+k", () => setSearchOpen(true), {
     preventDefault: true,
   });
   useHotkeys("mod+comma", () => setSettingsOpen(true), {
+    preventDefault: true,
+  });
+  useHotkeys("mod+shift+h", () => navigate("/"), {
     preventDefault: true,
   });
 
