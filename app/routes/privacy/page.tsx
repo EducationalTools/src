ce08b730e00bc1450e81fa136821fe988ef406dd
Ingo Wolf <me@ingowolf.au>
2025-11-19 13:32:31 +1100

    add privcy policy

--- app/routes/privacy/page.tsx
+++ app/routes/privacy/page.tsx
@@ -1,7 +1,8 @@
 export default function PrivacyPage() {
   return (
-    <div>
+    <div className="prose dark:prose-invert p-4 mx-auto">
       <h1>Privacy</h1>
+      Coming soon :)
     </div>
   );
 }
