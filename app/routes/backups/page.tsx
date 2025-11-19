3302535c65d66f32f4d3de1bee0aa8b6f385e423
Ingo Wolf <me@ingowolf.au>
2025-11-19 13:34:18 +1100

    add some stuff on backups page

--- app/routes/backups/page.tsx
+++ app/routes/backups/page.tsx
@@ -1,7 +1,7 @@
 export default function BackupsPage() {
   return (
-    <div>
-      <h1>Backups</h1>
+    <div className="max-w-4xl mx-auto w-full p-4 flex flex-col gap-4">
+      <h1 className="text-4xl">Backups</h1>
     </div>
   );
 }
