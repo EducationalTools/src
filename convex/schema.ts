a1dd2a82717cc18a8ea992f1fbcffe558da68172
Ingo Wolf <me@ingowolf.au>
2025-11-20 19:28:51 +1100

    format

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,12 +1,12 @@
-import { defineSchema, defineTable } from 'convex/server';
-import { v } from 'convex/values';
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
 
 export default defineSchema({
-	trustedMirrors: defineTable({
-		host: v.string()
-	}).index('by_host', ['host']),
-	profiles: defineTable({
-		userId: v.string(),
-		badges: v.optional(v.array(v.string()))
-	}).index('by_user', ['userId'])
+  trustedMirrors: defineTable({
+    host: v.string(),
+  }).index("by_host", ["host"]),
+  profiles: defineTable({
+    userId: v.string(),
+    badges: v.optional(v.array(v.string())),
+  }).index("by_user", ["userId"]),
 });
