5a8631e0b0633cb8d362437d37581ea87b42a60a
Ingo Wolf <me@ingowolf.au>
2025-11-24 09:34:58 +1100

    add animation

--- app/routes/backups/page.tsx
+++ app/routes/backups/page.tsx
@@ -23,6 +23,9 @@ import {
 } from "lucide-react";
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
+import { AnimatePresence, motion } from "motion/react";
+import { auth } from "@/convex/betterAuth/auth";
+import { NICE_EASE } from "@/lib/constants";
 
 export default function BackupsPage() {
   const [backupData, setBackupData] = useState("");
@@ -85,77 +88,94 @@ export default function BackupsPage() {
         </CardContent>
       </Card>
 
-      {session.data?.user &&
-      (convexAuth.isLoading || !cloudBackups?.success) ? (
-        <>
-          <Skeleton className="h-10 w-[150px]" />
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
-            <Skeleton className="h-24 w-full rounded-xl" />
-            <Skeleton className="h-24 w-full rounded-xl" />
-            <Skeleton className="h-24 w-full rounded-xl" />
-          </div>
-        </>
-      ) : (
-        <>
-          <h2 className="text-2xl">Cloud Backups</h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
-            <div className="border bg-card rounded-xl p-4 flex flex-col gap-2">
-              <h3 className="text-lg">Create Backup</h3>
-              <div className="flex flex-row gap-2">
-                <Input
-                  value={inputtedBackupName}
-                  onChange={(e) => setInputtedBackupName(e.target.value)}
-                  placeholder="Backup Name"
-                />
-                <Button
-                  size="icon"
-                  onClick={async () => {
-                    setLoading(true);
-                    const result = await createCloudBackup({
-                      data: createBackup(),
-                      name: inputtedBackupName,
-                    });
-                    if (result?.success) {
-                      toast.success("Backup created");
-                      setInputtedBackupName("");
-                    } else {
-                      toast.error("Failed to create backup");
-                    }
-                    setLoading(false);
-                  }}
-                >
-                  <ArrowRight />
-                </Button>
+      {session.data?.user && (
+        <AnimatePresence mode="popLayout" initial={false}>
+          {convexAuth.isLoading || !cloudBackups?.success ? (
+            <motion.div
+              initial={{ opacity: 0, y: 10 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -10 }}
+              transition={{ ease: NICE_EASE }}
+              key="loading"
+              className="flex flex-col gap-2"
+            >
+              <Skeleton className="h-10 w-[150px]" />
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+                <Skeleton className="h-24 w-full rounded-xl" />
+                <Skeleton className="h-24 w-full rounded-xl" />
+                <Skeleton className="h-24 w-full rounded-xl" />
               </div>
-            </div>
-            {cloudBackups?.success &&
-              cloudBackups?.backups?.map((backup) => (
-                <div
-                  key={backup._id}
-                  className="flex flex-col gap-2 bg-card rounded-xl p-4 border"
-                >
-                  <h3 className="text-lg">{backup.name}</h3>
-                  <div className="grow"></div>
-                  <div className="flex flex-row gap-2 justify-end">
-                    <Button variant="destructive" size="icon">
-                      <Trash />
-                    </Button>
-                    <Button variant="outline" size="icon">
-                      <Copy />
-                    </Button>
+            </motion.div>
+          ) : (
+            <motion.div
+              initial={{ opacity: 0, y: 10 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -10 }}
+              transition={{ ease: NICE_EASE }}
+              key="loaded"
+              className="flex flex-col gap-2"
+            >
+              <h2 className="text-2xl">Cloud Backups</h2>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+                <div className="border bg-card rounded-xl p-4 flex flex-col gap-2">
+                  <h3 className="text-lg">Create Backup</h3>
+                  <div className="flex flex-row gap-2">
+                    <Input
+                      value={inputtedBackupName}
+                      onChange={(e) => setInputtedBackupName(e.target.value)}
+                      placeholder="Backup Name"
+                    />
                     <Button
-                      onClick={() => {
-                        restoreBackup(backup.data);
+                      size="icon"
+                      onClick={async () => {
+                        setLoading(true);
+                        const result = await createCloudBackup({
+                          data: createBackup(),
+                          name: inputtedBackupName,
+                        });
+                        if (result?.success) {
+                          toast.success("Backup created");
+                          setInputtedBackupName("");
+                        } else {
+                          toast.error("Failed to create backup");
+                        }
+                        setLoading(false);
                       }}
                     >
-                      <ArchiveRestore />
-                      Restore
+                      <ArrowRight />
                     </Button>
                   </div>
                 </div>
-              ))}
-          </div>
-        </>
+                {cloudBackups?.success &&
+                  cloudBackups?.backups?.map((backup) => (
+                    <div
+                      key={backup._id}
+                      className="flex flex-col gap-2 bg-card rounded-xl p-4 border"
+                    >
+                      <h3 className="text-lg">{backup.name}</h3>
+                      <div className="grow"></div>
+                      <div className="flex flex-row gap-2 justify-end">
+                        <Button variant="destructive" size="icon">
+                          <Trash />
+                        </Button>
+                        <Button variant="outline" size="icon">
+                          <Copy />
+                        </Button>
+                        <Button
+                          onClick={() => {
+                            restoreBackup(backup.data);
+                          }}
+                        >
+                          <ArchiveRestore />
+                          Restore
+                        </Button>
+                      </div>
+                    </div>
+                  ))}
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
       )}
     </div>
   );
