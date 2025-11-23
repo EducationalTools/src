01ff001e87df907e596162f824b8311f97313f12
Ingo Wolf <me@ingowolf.au>
2025-11-24 10:21:00 +1100

    add autofocus

--- app/routes/backups/page.tsx
+++ app/routes/backups/page.tsx
@@ -172,6 +172,7 @@ export default function BackupsPage() {
                           handleCreateBackup();
                         }
                       }}
+                      autoFocus
                       placeholder="Backup Name"
                       disabled={loading}
                     />
