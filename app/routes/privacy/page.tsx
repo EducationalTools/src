6ef23f473450d6ecb898d0ad316df9de53305144
Ingo Wolf <me@ingowolf.au>
2025-11-24 11:15:30 +1100

    update

--- app/routes/privacy/page.tsx
+++ app/routes/privacy/page.tsx
@@ -2,7 +2,7 @@ export default function PrivacyPage() {
   return (
     <div className="prose dark:prose-invert p-4 mx-auto">
       <h1>Privacy</h1>
-      Coming soon :)
+      <p>Coming soon :)</p>
     </div>
   );
 }
