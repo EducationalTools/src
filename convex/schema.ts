17e98b6170782955278d51bc68f85b94226df372
Ingo Wolf <me@ingowolf.au>
2025-12-01 12:42:28 +1100

    remove profiles because future me problem

--- convex/schema.ts
+++ convex/schema.ts
@@ -5,10 +5,6 @@ export default defineSchema({
   trustedMirrors: defineTable({
     host: v.string(),
   }).index("by_host", ["host"]),
-  profiles: defineTable({
-    userId: v.string(),
-    badges: v.optional(v.array(v.string())),
-  }).index("by_user", ["userId"]),
 
   backups: defineTable({
     data: v.string(),
